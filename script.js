30aa783a2abb42ecf4165df53a30eb3eeec79bce
Rishi-Mishima <153814332+Rishi-Mishima@users.noreply.github.com>
2025-04-05 14:51:41 +0200

    update the grid slide range bar but the layout grid cannot be changed

--- script.js
+++ script.js
@@ -2,12 +2,31 @@ const container = document.querySelector("#container");
 
 const gridsize = 16;
 
+const slideRange = document.getElementById("gridSize");
+const gridValue = document.getElementById("gridValue");
+const gridValue2 = document.getElementById("gridValue2");
+
+slideRange.addEventListener('input', changeGrid);
+
+function changeGrid() {
+    const size = slideRange.value;
+    gridValue.textContent = size;
+    gridValue2.textContent = size;
+    recreateGrid(size);
+}
+
+
+
+//--------------------------------
+
 for (let i = 0; i < gridsize * gridsize; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
 
 }
+
+
 
 const squares = document.querySelectorAll(".square");
 squares.forEach(square => {
@@ -26,6 +45,7 @@ function changeColor() {
 function setRandomColor(e) {
     e.target.style.backgroundColor = changeColor();
 }
+
 function enableFadingTrail() {
     document.querySelectorAll(".square").forEach(square => {
         square.addEventListener("mouseover", () => {
@@ -46,3 +66,4 @@ function resetColor() {
         square.style.backgroundColor = "white";
     })
 }
+
