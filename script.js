cf1b6cd306763303f2ac0b79e194af72a8c44d06
Rishi-Mishima <153814332+Rishi-Mishima@users.noreply.github.com>
2025-04-05 13:59:32 +0200

    finished color change and the reset button

--- script.js
+++ script.js
@@ -8,3 +8,41 @@ for (let i = 0; i < gridsize * gridsize; i++) {
     container.appendChild(square);
 
 }
+
+const squares = document.querySelectorAll(".square");
+squares.forEach(square => {
+    square.addEventListener('mouseover', setRandomColor);
+})
+
+function changeColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function setRandomColor(e) {
+    e.target.style.backgroundColor = changeColor();
+}
+function enableFadingTrail() {
+    document.querySelectorAll(".square").forEach(square => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+        });
+        square.addEventListener("mouseout", () => {
+            square.style.backgroundColor = "white";
+        });
+    });
+}
+
+const reset = document.querySelector("button");
+
+reset.addEventListener("click", resetColor);
+
+function resetColor() {
+    squares.forEach(square => {
+        square.style.backgroundColor = "white";
+    })
+}
